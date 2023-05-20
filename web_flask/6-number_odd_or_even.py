4e1c7c227797544875109ceb6450291e22a30b6a
Lagatkipruto <kiprutowillie@gmail.com>
2023-05-20 16:52:28 +0000

    The script that display an html page

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application
+listening on 0.0.0.0, port 5000
+Routes:
+    / Display "Hello HBNB"
+    /hbnb Display "HBNB"
+    /c/<text> Display "c" followed by the value of the text variable
+    /python/(<text>) Display "Python"
+        followed by the value of the text variable
+    /number/<n> Display "n" is a number only if n is an integer
+    /number_template/<n> Display a HTML page only if n is an integer
+    /number_odd_or_even/<n> Display a HTML page only if n is an integer
+    H1 tag "Number: n is even|odd" inside the tag BODY
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Display "Hello HBNB"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display 'HBNB'"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C_text(text):
+    """Display "c" followed by the value of the text variable"""
+    return 'C {}'.format(escape(text.replace('_', ' ')))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """ Display "Python" followed by the value of the text variable"""
+    text = escape(text).replace('_', ' ')
+    return "Python {}".format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Display "n" is a number only if n is an integer"""
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """Display a HTML page only if n is an integer"""
+    if isinstance(n, int):
+        return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """Displays whether a number is odd or even"""
+    if n % 2 == 0:
+        parity = 'even'
+    else:
+        parity = 'odd'
+        return render_template('6-number_odd_or_even.html', n=n, parity=parity)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
