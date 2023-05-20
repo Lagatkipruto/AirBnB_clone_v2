427c691f2947d1bfc5c0ebdd7b729583689b169c
Lagatkipruto <kiprutowillie@gmail.com>
2023-05-20 15:21:24 +0000

    Correction on task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,16 @@
+#!/usr/bin/python3
+"""Starts Flask web app listening on 0.0.0.0:5000
+Route '/' displays "Hello HBNB!"
+"""
 from flask import Flask
+
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
 
